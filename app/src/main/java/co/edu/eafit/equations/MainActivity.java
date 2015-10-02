636307a1db5eb609a7ee5ee24d007142657eaa3a
Daniel Arboleda Sanchez <jarbol16@eafit.edu.co>
2015-10-02 12:00:23 -0500

    Tabla,Manejo deBigDecimal, BI

--- app/src/main/java/co/edu/eafit/equations/MainActivity.java
+++ app/src/main/java/co/edu/eafit/equations/MainActivity.java
@@ -1,23 +1,14 @@
 package co.edu.eafit.equations;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -64,7 +55,9 @@ public class MainActivity extends AppCompatActivity
                 fragment = new Approximations();
                 break;
             case 3: //Una Variable
-                fragment = new SingleVarible();
+                //fragment = new SingleVarible();
+                Intent act = new Intent(this,IncrementalSearches.class);
+                startActivity(act);
                 break;
         }
         fragmentManager.beginTransaction()
