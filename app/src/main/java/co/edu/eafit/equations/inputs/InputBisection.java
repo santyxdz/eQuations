c7c3f3466583e4617418e953d53d0d269c011e0d
Daniel Arboleda Sanchez <jarbol16@eafit.edu.co>
2015-10-05 08:14:56 -0500

    Implementados metodos

--- app/src/main/java/co/edu/eafit/equations/inputs/InputBisection.java
+++ app/src/main/java/co/edu/eafit/equations/inputs/InputBisection.java
@@ -5,9 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+
+import co.edu.eafit.equations.Methods;
 import co.edu.eafit.equations.R;
+import co.edu.eafit.equations.Tabs;
+import co.edu.eafit.equations.tables.TableBisection;
 
 public class InputBisection extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
@@ -15,9 +24,12 @@ public class InputBisection extends Fragment {
     public static InputBisection newInstance(int sectionNumber) {
         InputBisection fragment = new InputBisection();
         Bundle args = new Bundle();
-        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        //args.putInt(ARG_SECTION_NUMBER, sectionNumber);
         fragment.setArguments(args);
         return fragment;
+    }
+    public interface OnRefreshListener {
+        public void onRefresh();
     }
 
     public InputBisection() {
@@ -25,9 +37,54 @@ public class InputBisection extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.tab_input_bisection, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+        final View rootView = inflater.inflate(R.layout.tab_input_bisection, null);
+        final EditText inpt_function = (EditText)rootView.findViewById(R.id.inpt_function);
+        final EditText inpt_tol = (EditText)rootView.findViewById(R.id.inpt_tol);
+        final EditText inpt_xlower = (EditText)rootView.findViewById(R.id.inpt_xlower);
+        final EditText inpt_xtop = (EditText)rootView.findViewById(R.id.inpt_xtop);
+        final EditText inpt_iter = (EditText)rootView.findViewById(R.id.inpt_iter);
+        final TextView txt_push = (TextView) rootView.findViewById(R.id.txt_push);
+        final RadioGroup radioGroup = (RadioGroup) rootView.findViewById(R.id.RG);
+        final int[] tipeError = new int[1];
+        final RadioButton rdb_errorAb = (RadioButton) rootView.findViewById(R.id.rdb_errorAb);
+        final RadioButton rdb_errorRe = (RadioButton) rootView.findViewById(R.id.rdb_errorRe);
+
+        Button btn_run = (Button)rootView.findViewById(R.id.btn_run);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rdb_errorAb) {
+                    tipeError[0] = 1;
+                    txt_push.setText(R.string.absolute_error);
+                } else if(checkedId == R.id.rdb_errorRe){
+                    tipeError[0] = 0;
+                    txt_push.setText(R.string.relative_error);
+                }
+
+
+            }
+        });
+        btn_run.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Tabs) getActivity()).getTabla().getArray().clear();
+                BigDecimal x0 = new BigDecimal(inpt_xlower.getText().toString());
+                BigDecimal x1 = new BigDecimal(inpt_xtop.getText().toString());
+                BigDecimal tol = new BigDecimal(inpt_tol.getText().toString());
+                long iter = Integer.parseInt(inpt_iter.getText().toString());
+                ((Tabs) getActivity()).getTabla().addHead(
+                        getActivity().getResources().getStringArray(
+                                R.array.cabecera_tabla_Bisection
+                        )
+                );
+                Methods.Bisection(x0, x1, tol, iter, tipeError[0],
+                        ((Tabs) getActivity()).getTabla(),
+                        inpt_function.getText().toString()
+                );
+                ((TableBisection)((Tabs) getActivity()).getFragmentTable()).load_tables();
+                txt_push.setText(((Tabs)getActivity()).getTabla().getResult());
+            }
+        });
         return rootView;
     }
 }
--- app/src/main/java/co/edu/eafit/equations/inputs/InputFixedPoint.java
+++ app/src/main/java/co/edu/eafit/equations/inputs/InputFixedPoint.java
@@ -5,9 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+
+import co.edu.eafit.equations.Methods;
 import co.edu.eafit.equations.R;
+import co.edu.eafit.equations.Tabs;
+import co.edu.eafit.equations.tables.TableFixedPoint;
 
 public class InputFixedPoint extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
@@ -25,9 +33,50 @@ public class InputFixedPoint extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.tab_input_fixed_point, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+        final View rootView = inflater.inflate(R.layout.tab_input_fixed_point, null);
+        final EditText inpt_function = (EditText)rootView.findViewById(R.id.inpt_function);
+        final EditText inpt_function_gx = (EditText)rootView.findViewById(R.id.inpt_function_gx);
+        final EditText inpt_tol = (EditText)rootView.findViewById(R.id.inpt_tol);
+        final EditText inpt_xlower = (EditText)rootView.findViewById(R.id.inpt_xlower);
+        final EditText inpt_iter = (EditText)rootView.findViewById(R.id.inpt_iter);
+        final TextView txt_push = (TextView) rootView.findViewById(R.id.txt_push);
+        final RadioGroup radioGroup = (RadioGroup) rootView.findViewById(R.id.RG);
+        final int[] tipeError = new int[1];
+        Button btn_run = (Button)rootView.findViewById(R.id.btn_run);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rdb_errorAb) {
+                    tipeError[0] = 1;
+                    txt_push.setText(R.string.absolute_error);
+                } else if(checkedId == R.id.rdb_errorRe){
+                    tipeError[0] = 0;
+                    txt_push.setText(R.string.relative_error);
+                }
+
+
+            }
+        });
+        btn_run.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Tabs) getActivity()).getTabla().getArray().clear();
+                BigDecimal x0 = new BigDecimal(inpt_xlower.getText().toString());
+                BigDecimal tol = new BigDecimal(inpt_tol.getText().toString());
+                long iter = Integer.parseInt(inpt_iter.getText().toString());
+                ((Tabs) getActivity()).getTabla().addHead(
+                        getActivity().getResources().getStringArray(
+                                R.array.cabecera_tabla_FixedPoint
+                        )
+                );
+                Methods.FixedPoint(x0, tol, iter, tipeError[0], inpt_function.getText().toString(),
+                        inpt_function_gx.getText().toString(),((Tabs) getActivity()).getTabla()
+
+                );
+                ((TableFixedPoint)((Tabs) getActivity()).getFragmentTable()).load_tables();
+                txt_push.setText(((Tabs)getActivity()).getTabla().getResult());
+            }
+        });
         return rootView;
     }
 }
